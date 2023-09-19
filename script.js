e5f349ac5815ff504defda1e6a983e3e557ee639
anaswardeh <anaswardeh@gmail.com>
2023-09-18 20:10:30 -0400

    partial script file

---
+++ script.js
@@ -0,0 +1,12 @@
+// Initial Commit
+var formEl = $('#skills-form');
+var nameInputEl = $('#skill-name');
+var dateInputEl = $('#datepicker');
+var skillsListEl = $('#skills-list');
+
+var printSkills = function (name, date) {
+  var listEl = $('<li>');
+  var listDetail = name.concat(' on ', date);
+  listEl.addClass('list-group-item').text(listDetail);
+  listEl.appendTo(skillsListEl);
+};
